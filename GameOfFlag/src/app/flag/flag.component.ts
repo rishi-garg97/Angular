368801b525235af195b510313f132a893c43b5bc
rishi1107 <rishigargabu@gmail.com>
2018-06-06 01:52:14 +0530

    Game of Flag Code Completed

--- GameOfFlag/src/app/flag/flag.component.ts
+++ GameOfFlag/src/app/flag/flag.component.ts
@@ -12,29 +12,73 @@ export class FlagComponent implements OnInit {
   randomNationISOCode;
   myGuess;
   playing=false;
+ a= [];
+  canswer;
+  score=0;
+  correctPosition;
+  yes=false;
+  no=false;
+
 
   constructor(private cService: FlagServieService) { }
 
   ngOnInit() {
   }
 
-  getCountries(){
-    return ;
-  }
+ 
 
   next(){
+
     let countries = this.cService.getCountries();
     let random = Math.floor(Math.random()*countries.length);
-    this.randomNationName = countries[random].name;
+    this.canswer= this.randomNationName = countries[random].name;
     this.randomNationISOCode = countries[random]['alpha-2']
+ 
+    
     console.log(this.randomNationISOCode,this.randomNationName)
+
+
+    this.correctPosition =Math.floor(4*Math.random());
+    this.a[this.correctPosition] = this.canswer;
+
+    for(let i=0;i<4;i++){
+
+      if(i==this.correctPosition)
+      continue;
+
+      else{
+
+        let wanswer;
+
+        do   
+        {
+         
+        let countrie = this.cService.getCountries();
+        let ran = Math.floor(Math.random()*countrie.length);
+         wanswer= this.randomNationName = countries[ran].name;
+       }while(this.a.indexOf(wanswer)>-1);
+
+
+              this.a[i] = wanswer;
+      }
+    }
+
+  
+
     this.myGuess = "";
     this.playing=true;
     }
+
+
+
+
+
+
     onFinished(){
       this.playing=false;
       
-      alert("Ooops Timeout");
+      alert("Ooops Timeout Your score is "+ this.score);
+      location.reload();
     }
     onReset(){
       this.playing=false;
@@ -42,4 +86,34 @@ export class FlagComponent implements OnInit {
 
     }
 
+    check(aa){
+     
+     
+      if(aa==this.canswer){
+        
+        this.yes=true;
+        this.no=false;
+        
+        this.score++;
+        this.next();
+       
+      
+      }
+      else{
+       
+        this.no=true;
+        this.yes=false;
+        
+      }
+
+    }
+
+    onCorrect(){
+      this.yes=false;
+
+    }
+    onWrong(){
+      this.no=false;
+
+    }
 }
